6044161bbd430720d02b96d2b4736c9328abdf7d
Jeroenn89 <s1174675@student.windesheim.nl>
2026-02-20 13:03:09 +0100

    Starting road

--- scene.js
+++ scene.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import { createCube } from './Models/cube.js';
 import { createSkybox } from './Skybox/skybox.js';
+import { createRoad } from './Models/road.js';
 
 //idk of dit mag van de opdracht, maar is voor nu even handig ofz. Navragen bij Ward
 import { OrbitControls } from 'https://unpkg.com/three@0.171.0/examples/jsm/controls/OrbitControls.js';
+
 
 
 const scene = new THREE.Scene();
@@ -16,27 +18,19 @@ const renderer = new THREE.WebGLRenderer();
 
 //Orbitcontrols
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0.1, 0, 0);
 controls.update();
 
 renderer.setSize( window.innerWidth, window.innerHeight ); //set size of renderer to size of window
 document.body.appendChild( renderer.domElement );
 
-const cube1 = createCube(0x00ff00); // groene cube
-cube1.position.set(0, 0, 0);
-scene.add(cube1);
+const road = createRoad(0x5c5c5c);
+scene.add(road);
+road.position.y = -1;
 
-const cube2 = createCube(0xff0000); // rode cube
-cube2.position.set(3, 0, 0);
-scene.add(cube2);
-
-camera.position.z = 5;
+camera.position.set(0, 0, 0);
 
 function animate( time ) {
-    cube1.rotation.x = time / 2000;
-    cube1.rotation.y = time / 1000;
-    cube2.rotation.x = time / 2000;
-    cube2.rotation.y = time / 1000;
-
   controls.update();  //Orbitcontrols
 
   renderer.render( scene, camera );
